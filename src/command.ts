11d6509fa386133104dd33b3d5d41e457315ee47
fcapolini <fabrizio.capolini@me.com>
2023-09-13 17:05:55 +0200

    TR-78 - trillo CLI, serve <path> command

--- src/command.ts
+++ src/command.ts
@@ -1,14 +1,35 @@
 #!/usr/bin/env node
 
+import { Command } from 'commander';
 import { Server } from "./server";
+import path from "path";
 
-new Server({
-  port: 3001,
-  rootPath: process.cwd(),
-  trustProxy: false,
-  assumeHttps: false,
-  pageLimit: {
-    windowMs: 5000,
-    maxRequests: 50
-  }
-});
+const program = new Command();
+
+program
+  .name('trillo')
+  .description('Trillo CLI - https://trillojs.dev')
+  .version('1.0.0');
+
+program.command('serve')
+  .description('serve a Trillo project')
+  .argument('<pathname>', 'path to docroot')
+  .option('-p, --port <number>', 'port number, default: 3000')
+  // .option('-l, --live', 'enable auto reload on page changes')
+  .action((pathname, options) => {
+    const root = path.normalize(path.join(process.cwd(), pathname));
+    const port = parseInt(options.port) || 3000;
+    // const live = options.live || false;
+    new Server({
+      port: port,
+      rootPath: root,
+      trustProxy: false,
+      assumeHttps: false,
+      pageLimit: {
+        windowMs: 5000,
+        maxRequests: 50
+      }
+    });
+  });
+
+program.parse();
